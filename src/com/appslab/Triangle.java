fe90fb0f21a4e50550bb940ec4dcef069de159b4
Kultan Juraj <kultanj1@gmail.com>
2020-09-30 18:22:29 +0200

    Hello, his is my third excersice.

---
+++ src/com/appslab/Triangle.java
@@ -0,0 +1,16 @@
+package com.appslab;
+
+public class Triangle {
+    public static void main (String[] args){
+        double SiteA = 2;
+        double SiteB = 3;
+        double SiteC = 4;
+        if (SiteA==SiteB+SiteC&&SiteA+SiteB==SiteC&&SiteB==SiteA+SiteB){
+            boolean triangle = true;
+
+        }
+        else {
+          boolean triangle = false;
+        }
+    }
+}
