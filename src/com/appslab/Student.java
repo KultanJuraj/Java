ded062fba865729824c39e96cb96cb9ea06b3d23
Kultán Juraj <kultanj1@gmail.com>
2020-10-05 19:32:24 +0200

    My second exercise with objects.

--- src/com/appslab/Student.java
+++ src/com/appslab/Student.java
@@ -1,22 +1,28 @@
 package com.appslab;
 
 public class Student {
-    private String name;
+    private String address;
     private int studentNumber;
+    private String pNumber;
     public static void main(String[] args){
         Student student1 = new Student();
-        student1.setName("John");
-        student1.setStudentNumber(12345);
-        System.out.println(student1.getName()+":\t"+student1.getStudentNumber());
-
+        Student student2 = new Student();
+        student1.setStudentNumber(6);
+        student1.setpNumber("0918355642");
+        student1.setAddress("Zlámaná, 02351 Ráková");
+        student2.setStudentNumber(14);
+        student2.setpNumber("0916233455");
+        student2.setAddress("Solinky, 01001 Žilina");
+        System.out.println(student1.getStudentNumber() + ", " + student1.getpNumber() + ", " + student1.getAddress());
+        System.out.println(student2.getStudentNumber() + ", " + student2.getpNumber() + ", " + student2.getAddress());
     }
 
-    public String getName() {
-        return name;
+    public String getAddress() {
+        return address;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public int getStudentNumber() {
@@ -25,6 +31,12 @@ public class Student {
 
     public void setStudentNumber(int studentNumber) {
         this.studentNumber = studentNumber;
+    }
+    public String getpNumber(){
+        return pNumber;
+    }
+    public String setpNumber(String pNumber){
+        return pNumber=pNumber;
     }
 }
 
